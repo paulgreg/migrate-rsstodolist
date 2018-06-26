f0ffb7a3839957d87f4fb8a145a0e8366ebfd026
Grégory Paul <paulgreg@gmail.com>
2018-06-26 18:03:29 +0200

    Initial script

---
+++ index.js
@@ -0,0 +1,76 @@
+const rp = require('request-promise-native')
+const parse = require('xml2js').parseString
+const throat = require('throat')
+const config = require('./config.json')
+const max = 100
+
+function getReadUrl (base, feed) {
+    return `${base}?l=${max}&n=${feed}`
+}
+function getWriteUrl(base, feed, url) {
+    return `${base}add?n=${feed}&u=${url}`
+}
+
+let nbError = 0
+
+function getXmlFeed (base, feed) {
+    const url = getReadUrl(base, feed)
+    return rp(url)
+    .then(xml => {
+        console.log(`< fetching ${url}`)
+        return new Promise((resolve, reject) => {
+            parse(xml, (err, result) => {
+                if (err) return reject(err)
+                return resolve(result.rss.channel && result.rss.channel.length && result.rss.channel[0].item || [])
+            })
+        })
+    })
+}
+
+function alreadyInTarget(array, link) {
+    return array.indexOf(link) !== -1
+}
+
+config.feeds.map(feed => {
+
+    let itemsInTarget
+    return getXmlFeed(config.target, feed)
+    .then(items => {
+        itemsInTarget = items.map(item => item.link[0])
+    })
+    .then(() => getXmlFeed(config.source, feed))
+    .then(items => {
+        return items.map(item => {
+            return item.link[0].replace('\n', '')
+        })
+    })
+    .then(links => {
+        return Promise.all(links.map(throat(2, link => {
+            if (alreadyInTarget(itemsInTarget, link)) {
+                console.log(`= skipping link already in target : ${link}`)
+                return Promise.resolve()
+            } else {
+                const url = getWriteUrl(config.target, feed, link)
+                console.log(`> sending ${url}`)
+                return rp({
+                    method: 'GET',
+                    uri: url,
+                    resolveWithFullResponse: true,
+                })
+                .then(response => {
+                    console.log(`< reponse: ${response.statusCode} - ${url}`)
+                })
+                .catch(e => {
+                    nbError++
+                    console.error(`< reponse: ${response.statusCode} - ${url}`)
+                    if (nbError > 10) throw new Error('too much errors')
+                })
+            }
+        })))
+    })
+    .catch(e => {
+        console.error(e)
+        process.exit(1)
+    })
+})
+
