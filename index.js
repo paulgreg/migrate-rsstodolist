8d5c717ba42885c76d882c3b102f8ed2dc715910
Grégory Paul <paulgreg@gmail.com>
2018-06-27 20:45:06 +0200

    Use sequential to limit feed upload...
    and add a delay after adding an URL
    and add a cleanUrl method to remove utm parameters

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const rp = require('request-promise-native')
 const parse = require('xml2js').parseString
+const sequential = require('promise-sequential');
 const throat = require('throat')
 const config = require('./config.json')
 const max = 100
@@ -31,46 +32,56 @@ function alreadyInTarget(array, link) {
     return array.indexOf(link) !== -1
 }
 
-config.feeds.map(feed => {
+function cleanUrl (url) {
+    return url.replace(/&?utm_.+?(&|$)/g, '')
+}
 
+sequential(config.feeds.map(feed => {
     let itemsInTarget
-    return getXmlFeed(config.target, feed)
-    .then(items => {
-        itemsInTarget = items.map(item => item.link[0])
-    })
-    .then(() => getXmlFeed(config.source, feed))
-    .then(items => {
-        return items.map(item => {
-            return item.link[0].replace('\n', '')
+    return () => {
+        return getXmlFeed(config.target, feed)
+        .then(items => {
+            itemsInTarget = items.map(item => item.link[0])
         })
-    })
-    .then(links => {
-        return Promise.all(links.map(throat(2, link => {
-            if (alreadyInTarget(itemsInTarget, link)) {
-                console.log(`= skipping link already in target : ${link}`)
-                return Promise.resolve()
-            } else {
-                const url = getWriteUrl(config.target, feed, link)
-                console.log(`> sending ${url}`)
-                return rp({
-                    method: 'GET',
-                    uri: url,
-                    resolveWithFullResponse: true,
-                })
-                .then(response => {
-                    console.log(`< reponse: ${response.statusCode} - ${url}`)
-                })
-                .catch(e => {
-                    nbError++
-                    console.error(`< reponse: ${e} - ${url}`)
-                    if (nbError > 10) throw new Error('too much errors')
-                })
-            }
-        })))
-    })
-    .catch(e => {
-        console.error(e)
-        process.exit(1)
-    })
-})
+        .then(() => getXmlFeed(config.source, feed))
+        .then(items => {
+            return items.map(item => {
+                return item.link[0].replace('\n', '')
+            })
+        })
+        .then(links => {
+            return Promise.all(links.map(throat(2, rawLink => {
+                const link = cleanUrl(rawLink)
+                if (alreadyInTarget(itemsInTarget, link)) {
+                    console.log(`= skipping link already in target : ${link}`)
+                    return Promise.resolve()
+                } else {
+                    const url = getWriteUrl(config.target, feed, link)
+                    console.log(`> sending ${url}`)
+                    return rp({
+                        method: 'GET',
+                        uri: url,
+                        resolveWithFullResponse: true,
+                    })
+                    .then(response => {
+                        console.log(`< reponse: ${response.statusCode} - ${url}`)
+                    })
+                    .catch(e => {
+                        nbError++
+                        console.error(`< reponse: ${e} - ${url}`)
+                        if (nbError > 100) throw new Error('too much errors')
+                    })
+                    .then(_ => {
+                        return new Promise(resolve => {
+                            setTimeout(resolve, 500)
+                        })
+                    })
+                }
+            })))
+        })
+        .catch(e => {
+            console.error(e)
+            process.exit(1)
+        })
+}}))
 
